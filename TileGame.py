e80725c73456903454cd3fbb0b8f6cb82eb9197c
My Thanh Thao Nguyen <mythanhthaonguyen@Mys-MacBook-Pro.local>
2022-03-07 11:48:12 -0600

    modify the driver

--- TileGame.py
+++ TileGame.py
@@ -9,6 +9,18 @@ def main():
         for i in range(0, len(lines), 2):
             start = lines[i].split(" ")
             goal = lines[i+1].split(" ")
+            n = Node(start, None, "Start", True)
+            if n.solvable(goal):
+                print("woohoo solvable!")
+                #TODO implemented BFGS
+                #TODO implemented DFS
+                #TODO implemented A* search
+            else:
+                print("Unsolvable")
+        return
+    else:
+        print("Invalid input file")
+        return
 
 
 
